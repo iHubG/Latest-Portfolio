3cb17c3718591664c0b61bab059ef1d9946f550c
iHubG <ianmacalinao50@gmail.com>
2024-03-27 21:00:15 +0800

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let btn = document.querySelector('#theme');
+let darkMode = document.body;
+let image =  document.querySelector('#theme');
+let user = document.querySelector('#user');
+
+btn.addEventListener('click', function onClick(event) {
+
+    // changing moon into light bulb
+    if (image.src.match("img/icons8-moon-100.png")) {
+        image.src = "img/icons8-light-100.png";
+    }
+    else {
+        image.src = "img/icons8-moon-100.png";
+    }
+
+    // changing black into white
+    if (user.src.match("img/black.png")) {
+        user.src = "img/white.png";
+    }
+    else {
+        user.src = "img/black.png";
+    }
+
+    darkMode.classList.toggle('dark-mode'); 
+    
+});
+
+
+
+
