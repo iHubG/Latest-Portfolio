4be8c100c44bb8119edb698e5e6d267f1425e33e
iHubG <ianmacalinao50@gmail.com>
2024-03-28 19:39:32 +0800

    commit

--- script.js
+++ script.js
@@ -26,5 +26,9 @@ btn.addEventListener('click', function onClick(event) {
 });
 
 
+//Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 
